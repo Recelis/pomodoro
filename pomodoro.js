f5491f57274b38b05424cf183b1c9447219c1583
Jacky <jackymlui@live.com>
2017-12-27 14:09:24 +1000

    adding firebase to track pomodoros per day

--- pomodoro.js
+++ pomodoro.js
@@ -19,127 +19,135 @@ view{
 
 // global value
 var timer;
-var breakSound = new Audio('https://s3.amazonaws.com/freecodecamp/simonSound1.mp3');
-var pomoSound = new Audio('https://s3.amazonaws.com/freecodecamp/simonSound2.mp3');
-
+var breakSound = new Audio("https://s3.amazonaws.com/freecodecamp/simonSound1.mp3");
+var pomoSound = new Audio("https://s3.amazonaws.com/freecodecamp/simonSound2.mp3");
 
 var data = {
-    sessionLeft:1500,
-    breakLeft:300,
-    sessionLength: 1500,
-    breakLength:300,
-    status:'session',
-    timeElapsed: ()=>{
-        if (data.status == 'session') {
-            data.sessionLeft--;
-            if (data.sessionLeft == 0){
-                data.breakLeft = data.breakLength;
-                breakSound.play();
-                data.status = 'break';
-            }
-            var timeLeft = data.convertToMinutes(data.sessionLeft);
-        }
-        else if (data.status == 'break'){
-            data.breakLeft--;
-            if (data.breakLeft == 0){
-                data.sessionLeft = data.sessionLength;
-                pomoSound.play();
-                data.status = 'session';
-            }
-            var timeLeft = data.convertToMinutes(data.breakLeft);
-        } else{ // pause
-            var timeLeft = data.convertToMinutes(data.sessionLeft);
-        } 
-        view.updateClock(data.status, timeLeft);
-    },
-    convertToMinutes:(seconds)=>{
-        var minutes = Math.floor(seconds/60);
-        var remainder = seconds%60;
-        if (remainder < 10) return (minutes + ':0'+remainder).toString();
-        return (minutes + ':'+remainder).toString();
-    }
-}
+	sessionLeft:15,
+	breakLeft:300,
+	sessionLength: 1500,
+	breakLength:300,
+	status:"session",
+	timeElapsed: ()=>{
+		if (data.status == "session") {
+			data.sessionLeft--;
+			firebase.database().ref("counter").once("value").then((snapshot)=>{
+				let count = snapshot.val();
+				console.log(count);
+			});
+			if (data.sessionLeft == 0){
+				data.breakLeft = data.breakLength;
+				breakSound.play();
+				data.status = "break";
+				firebase.database().ref("counter").once("value").then((snapshot)=>{
+					let count = snapshot.val();
+					count++;
+					firebase.database().ref("counter").set(count);
+				});
+			}
+			var timeLeft = data.convertToMinutes(data.sessionLeft);
+		}
+		else if (data.status == "break"){
+			data.breakLeft--;
+			if (data.breakLeft == 0){
+				data.sessionLeft = data.sessionLength;
+				pomoSound.play();
+				data.status = "session";
+			}
+			var timeLeft = data.convertToMinutes(data.breakLeft);
+		} else{ // pause
+			var timeLeft = data.convertToMinutes(data.sessionLeft);
+		} 
+		view.updateClock(data.status, timeLeft);
+	},
+	convertToMinutes:(seconds)=>{
+		var minutes = Math.floor(seconds/60);
+		var remainder = seconds%60;
+		if (remainder < 10) return (minutes + ":0"+remainder).toString();
+		return (minutes + ":"+remainder).toString();
+	}
+};
 
 var handler = {
-    running:false,
-    lastStatus:'session',
-    time: ()=>{
-        if (handler.running == false){
-            handler.running = true;
-            data.status = handler.lastStatus;
-            timer = setInterval(()=>data.timeElapsed(),1000);
-            pomoSound.play();
-        }
-        else{
-            handler.running = false;
-            clearInterval(timer);
-            handler.lastStatus = data.status;
-            data.status = 'pause';
-            data.timeElapsed();
-        }
-    },
-    restart:()=>{
-        handler.running = false;
-        clearInterval(timer);
-        data.breakLeft = data.breakLength;
-        data.sessionLeft = data.sessionLength;
-        data.status = 'session';
-        view.updateClock(data.status, "Start!");
-        pomoSound.play();
-    },
-    changeBreakLength:(object)=>{
-        if (handler.running != true){
-            if (object.id == 'decreaseBreak') {
-                data.breakLength-=60;
-                if (data.breakLength < 0) data.breakLength == 0;
-            }
-            if (object.id == 'increaseBreak') data.breakLength+=60;
-            handler.restart();
-            view.updateLengths();
-        }
-    },
-    changeSessionLength:(object)=>{
-        if (handler.running != true){
-            if (object.id == 'decreaseSession') {
-                data.sessionLength-=60;
-                if (data.sessionLength < 0) data.sessionLength = 0;
-            }
-            if (object.id == 'increaseSession') data.sessionLength+=60;
-            handler.restart();
-            view.updateLengths();
-        }
-    }
+	running:false,
+	lastStatus:"session",
+	time: ()=>{
+		if (handler.running == false){
+			handler.running = true;
+			data.status = handler.lastStatus;
+			timer = setInterval(()=>data.timeElapsed(),1000);
+			pomoSound.play();
+		}
+		else{
+			handler.running = false;
+			clearInterval(timer);
+			handler.lastStatus = data.status;
+			data.status = "pause";
+			data.timeElapsed();
+		}
+	},
+	restart:()=>{
+		handler.running = false;
+		clearInterval(timer);
+		data.breakLeft = data.breakLength;
+		data.sessionLeft = data.sessionLength;
+		data.status = "session";
+		view.updateClock(data.status, "Start!");
+		pomoSound.play();
+	},
+	changeBreakLength:(object)=>{
+		if (handler.running != true){
+			if (object.id == "decreaseBreak") {
+				data.breakLength-=60;
+				if (data.breakLength < 0) data.breakLength == 0;
+			}
+			if (object.id == "increaseBreak") data.breakLength+=60;
+			handler.restart();
+			view.updateLengths();
+		}
+	},
+	changeSessionLength:(object)=>{
+		if (handler.running != true){
+			if (object.id == "decreaseSession") {
+				data.sessionLength-=60;
+				if (data.sessionLength < 0) data.sessionLength = 0;
+			}
+			if (object.id == "increaseSession") data.sessionLength+=60;
+			handler.restart();
+			view.updateLengths();
+		}
+	}
 
-}
+};
 
 var view = {
-    heightFill:100,
-    updateClock:(status, timeLeft)=>{
-        var timeLeftHandler = document.getElementById('timeLeft');
-        timeLeftHandler.innerHTML = timeLeft; 
-        var statusHandler = document.getElementById('status');
-        switch(status){
-            case 'session':
-                statusHandler.innerHTML = 'Session';
-                break;
-            case 'break':
-                statusHandler.innerHTML = 'Break';
-                break;
-            case 'pause':
-                statusHandler.innerHTML = 'Paused';
-                break;
-        }
-        // fill clock
-        view.calculateFillHeight();
-        document.getElementById('fill').style.height = (view.heightFill + '%').toString();
-    },
-    calculateFillHeight:()=>{
-        view.heightFill = Math.floor(data.sessionLeft/data.sessionLength*100);
-    },
-    updateLengths:()=>{
-        document.getElementById("breakTime").innerHTML = data.convertToMinutes(data.breakLength);
-        document.getElementById("sessionTime").innerHTML = data.convertToMinutes(data.sessionLength);        
-    },
-}
+	heightFill:100,
+	updateClock:(status, timeLeft)=>{
+		var timeLeftHandler = document.getElementById("timeLeft");
+		timeLeftHandler.innerHTML = timeLeft; 
+		var statusHandler = document.getElementById("status");
+		switch(status){
+		case "session":
+			statusHandler.innerHTML = "Session";
+			break;
+		case "break":
+			statusHandler.innerHTML = "Break";
+			break;
+		case "pause":
+			statusHandler.innerHTML = "Paused";
+			break;
+		}
+		// fill clock
+		view.calculateFillHeight();
+		document.getElementById("fill").style.height = (view.heightFill + "%").toString();
+	},
+	calculateFillHeight:()=>{
+		view.heightFill = Math.floor(data.sessionLeft/data.sessionLength*100);
+	},
+	updateLengths:()=>{
+		document.getElementById("breakTime").innerHTML = data.convertToMinutes(data.breakLength);
+		document.getElementById("sessionTime").innerHTML = data.convertToMinutes(data.sessionLength);        
+	},
+};
 
 
